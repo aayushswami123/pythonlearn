1cabb670488d95f5d832e633263625aaacc627ca
Aayush Swami <aa>
2025-03-30 11:46:40 -0700

    Initial commit

---
+++ Variable.py
@@ -0,0 +1,21 @@
+# Variable IS a RE-usable container for the values ( String , Int , Float, Boolean ) it beghaves as if it contains something in itself 
+
+#string 
+first_mame = "aayush "
+print(first_mame) # to print the first name 
+print(f"hello {first_mame}") # to printy the fist namrre in f type format 
+
+# Int 
+age = 25 
+quantity = 3
+
+#float
+price = 25.00
+age = 17.30
+
+# boolean
+is_student = True
+if is_student:
+    print("you are a Student")
+else:
+    print("you are NOT a student ")
---
+++ typecast.py
@@ -0,0 +1,18 @@
+# Type casting - the process of converting a variable from one data type to another 
+#         str(), int(), float(), bool()
+
+name= "aayush swami"
+age = 18
+gpa = 3.89
+is_student =True
+
+# type() to get the date type of the variable 
+# gpa(float) to -----> int 
+gpa = int(gpa)
+print(gpa)
+
+# age to ----> float value ( int to float )
+age = float(age)
+print(age)
+
+print(type(age))
